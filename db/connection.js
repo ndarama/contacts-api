dbaef6d23ff071f59ceeb48252612efa89146f48
Ndarama <ndaramartwork@gmail.com>
2025-01-23 17:57:06 +0200

    DB update v1

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        const uri = process.env.MONGO_URI;
+        if (!uri) throw new Error('MONGO_URI is not defined');
+        
+        await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
